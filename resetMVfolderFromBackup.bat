52b1e0113bbda2450536d64ff1e1979c4e8f5bf7
Epirasque <romanhable@web.de>
2022-02-16 12:43:32 +0100

    add supporting batch files

---
+++ resetMVfolderFromBackup.bat
@@ -0,0 +1,3 @@
+@RD /S /Q "C:\Users\roman\PycharmProjects\glaive\FTL-Multiverse 5.1 Hotfix"
+
+robocopy  "C:\Users\roman\PycharmProjects\glaive\FTL-Multiverse 5.1 Hotfix - BACKUP" "C:\Users\roman\PycharmProjects\glaive\FTL-Multiverse 5.1 Hotfix" /COPYALL /E
