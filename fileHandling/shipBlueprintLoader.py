0233f0da3741849eba17bfe9c3ef2eb21a84a396
Epirasque <romanhable@web.de>
2022-02-25 22:01:54 +0100

    improved initial ship blueprint loading

--- fileHandling/shipBlueprintLoader.py
+++ fileHandling/shipBlueprintLoader.py
@@ -1,20 +1,15 @@
 import xml.etree.ElementTree as ET
 from os.path import exists
+import re
 
 SHIP_BLUEPRINT_ATTRIBUTE = 'shipBlueprint'
 
 
 def loadShipFileNames(sourceFolderpath):
     blueprints = []
-    if exists(sourceFolderpath + "\\data\\blueprints.xml.append") == True:
-        parsed = ET.parse(sourceFolderpath + "\\data\\blueprints.xml.append")
-        blueprints.extend(parsed.getroot().findall(SHIP_BLUEPRINT_ATTRIBUTE))
-    if exists(sourceFolderpath + "\\data\\autoBlueprints.xml.append") == True:
-        parsed = ET.parse(sourceFolderpath + "\\data\\autoBlueprints.xml.append")
-        blueprints.extend(parsed.getroot().findall(SHIP_BLUEPRINT_ATTRIBUTE))
-    if exists(sourceFolderpath + "\\data\\bosses.xml.append") == True:
-        parsed = ET.parse(sourceFolderpath + "\\data\\bosses.xml.append")
-        blueprints.extend(parsed.getroot().findall(SHIP_BLUEPRINT_ATTRIBUTE))
+    addBlueprintsFromFile(blueprints, sourceFolderpath, 'blueprints.xml.append')
+    addBlueprintsFromFile(blueprints, sourceFolderpath, 'autoBlueprints.xml.append')
+    addBlueprintsFromFile(blueprints, sourceFolderpath, 'bosses.xml.append')
 
     ships = {}
     for blueprint in blueprints:
@@ -23,3 +18,13 @@ def loadShipFileNames(sourceFolderpath):
         ships[blueprint.attrib['name']]['layout'] = blueprint.attrib['layout']
 
     return ships
+
+
+def addBlueprintsFromFile(blueprints, sourceFolderpath, filename):
+    if exists(sourceFolderpath + '\\data\\' + filename) == True:
+        with open(sourceFolderpath + '\\data\\' + filename, encoding='utf-8') as file:
+            rawXml = file.read()
+        # TODO: get rid of all <mod:...> sections
+        validXmlString =  '<root>' + re.sub(r"(<\?xml[^>]+\?>)", r"\1", rawXml) + '</root>'
+        parsed = ET.ElementTree(ET.fromstring(validXmlString))
+        blueprints.extend(parsed.getroot().findall(".//" + SHIP_BLUEPRINT_ATTRIBUTE))
