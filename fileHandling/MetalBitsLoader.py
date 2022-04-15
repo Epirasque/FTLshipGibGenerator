dabd51d0664f5a7c7b40ad3f64b26751e893921d
Epirasque <romanhable@web.de>
2022-04-15 09:24:52 +0200

    auto-rotate tiles to extend 0°-to-90°-input to 0°-to-360°

--- fileHandling/MetalBitsLoader.py
+++ fileHandling/MetalBitsLoader.py
@@ -8,13 +8,14 @@ from imageProcessing.ImageProcessingUtilities import findColorInImage, findMeanO
 
 logger = logging.getLogger('GLAIVE.' + __name__)
 
-LAYER1 = 'chunks'
-DEFAULT_TILESET = 'placeholder/arrow'
-TILE_WIDTH = 10 #72
-TILE_HEIGHT = 10 #72
+LAYER1 = 'layer1'
+DEFAULT_TILESET = 'default/10x10_metal_bits_1'
+TILE_WIDTH = 10  # 72
+TILE_HEIGHT = 10  # 72
 # should divide 360 without remainder
-CLOCKWISE_ANGLE_PER_STEP = 45 #15
+CLOCKWISE_ANGLE_PER_STEP = 45  # 15
 NR_ANGLE_STEPS = round(360 / CLOCKWISE_ANGLE_PER_STEP)
+NR_ANGLE_STEPS_WHEN_LOADING = round(NR_ANGLE_STEPS / 4)
 ORIGIN_COLOR = [0, 255, 0]
 
 
@@ -41,7 +42,7 @@ def splitIntoDictionary(imageArray, nrPieces):
     tilesets['default'][LAYER1] = []
     for pieceID in range(nrPieces):
         angleDict = {}
-        for angleStep in range(NR_ANGLE_STEPS):
+        for angleStep in range(NR_ANGLE_STEPS_WHEN_LOADING):
             yMin = angleStep * TILE_HEIGHT
             yMax = (angleStep + 1) * TILE_HEIGHT  # -1 implicitly given by ':' range
             xMin = pieceID * TILE_WIDTH
@@ -50,19 +51,27 @@ def splitIntoDictionary(imageArray, nrPieces):
             angle = angleStep * CLOCKWISE_ANGLE_PER_STEP
             angleDict[angle] = {}
             angleDict[angle]['img'] = np.ma.copy(tile)
+            angleDict[angle + 270] = {}
+            angleDict[angle + 270]['img'] = np.rot90(np.ma.copy(angleDict[angle + 0]['img']))
+            angleDict[angle + 180] = {}
+            angleDict[angle + 180]['img'] = np.rot90(np.ma.copy(angleDict[angle + 270]['img']))
+            angleDict[angle + 90] = {}
+            angleDict[angle + 90]['img'] = np.rot90(np.ma.copy(angleDict[angle + 180]['img']))
+
         tilesets['default'][LAYER1].append(angleDict)
     return tilesets
 
 
 def verifyTilesetDimensions(imageArray, tilesetFilePath):
     ymax, xmax = imageArray.shape[0], imageArray.shape[1]
-    if (ymax / TILE_HEIGHT != NR_ANGLE_STEPS):
+    if (ymax / TILE_HEIGHT != NR_ANGLE_STEPS_WHEN_LOADING):
         logger.critical('ERROR when reading tileset %s, tileset height does not match expected %u' % (
             tilesetFilePath, TILE_HEIGHT * (360 / CLOCKWISE_ANGLE_PER_STEP)))
         exit(1)
     nrPieces = xmax / TILE_WIDTH
     if nrPieces != round(nrPieces):
-        logger.critical('ERROR when reading tileset %s, tileset width is not multiple of %u' % (tilesetFilePath, TILE_WIDTH))
+        logger.critical(
+            'ERROR when reading tileset %s, tileset width is not multiple of %u' % (tilesetFilePath, TILE_WIDTH))
         exit(1)
     return round(nrPieces)
 
