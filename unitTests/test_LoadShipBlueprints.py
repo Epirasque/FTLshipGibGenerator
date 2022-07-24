9c27f34fc39d9ac7c7f34bab77b304e5f20eb316
Epirasque <romanhable@web.de>
2022-07-24 08:43:29 +0200

    Update to fix: test_LoadShipBlueprints

--- unitTests/test_LoadShipBlueprints.py
+++ unitTests/test_LoadShipBlueprints.py
@@ -9,26 +9,35 @@ class LoadShipBlueprintsTest(unittest.TestCase):
         initializeLoggingForTest(self)
 
     def test_loadShipBlueprintsWithXmlAndFtlTagsWithoutErrors(self):
-        ships = loadShipFileNames('sampleProjects/XML_and_FTL_tags')
+        ships, layoutUsages = loadShipFileNames('sampleProjects/XML_and_FTL_tags')
         self.assertCorrectShipNames(ships)
+        self.assertCorrectLayoutUsages(layoutUsages)
 
     def test_loadShipBlueprintsWithXmlTagsWithoutErrors(self):
-        ships = loadShipFileNames('sampleProjects/XML_tags')
+        ships, layoutUsages = loadShipFileNames('sampleProjects/XML_tags')
         self.assertCorrectShipNames(ships)
+        self.assertCorrectLayoutUsages(layoutUsages)
 
     def test_loadShipBlueprintsWithFtlTagsWithoutErrors(self):
-        ships = loadShipFileNames('sampleProjects/FTL_tags')
+        ships, layoutUsages = loadShipFileNames('sampleProjects/FTL_tags')
         self.assertCorrectShipNames(ships)
+        self.assertCorrectLayoutUsages(layoutUsages)
 
     def test_loadShipBlueprintsWithNoTagsWithoutErrors(self):
-        ships = loadShipFileNames('sampleProjects/no_tags')
+        ships, layoutUsages = loadShipFileNames('sampleProjects/no_tags')
         self.assertCorrectShipNames(ships)
+        self.assertCorrectLayoutUsages(layoutUsages)
 
     def assertCorrectShipNames(self, ships):
         assert len(ships) == 3
         assert 'TEST_SHIP1' in ships
         assert 'TEST_SHIP2' in ships
         assert 'TEST_SHIP3' in ships
+
+    def assertCorrectLayoutUsages(self, layoutUsages):
+        assert len(layoutUsages) == 2
+        assert layoutUsages['test_layoutA'] == 1
+        assert layoutUsages['test_layoutB'] == 2
 
 
 if __name__ == '__main__':
