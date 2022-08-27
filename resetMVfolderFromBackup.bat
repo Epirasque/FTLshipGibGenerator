31bcdd92a5547ffa76237060ad1995215f5f90b4
Epirasque <romanhable@web.de>
2022-08-27 22:39:38 +0200

    source folder not playtest anymore

--- resetMVfolderFromBackup.bat
+++ resetMVfolderFromBackup.bat
@@ -1,3 +1,3 @@
-@RD /S /Q "C:\Users\roman\PycharmProjects\glaive\projects\FTL-Multiverse 5.3 playtest"
+@RD /S /Q "C:\Users\roman\PycharmProjects\glaive\projects\FTL-Multiverse 5.3"
 
-robocopy  "C:\Users\roman\PycharmProjects\glaive\projects\FTL-Multiverse 5.3 playtest - BACKUP" "C:\Users\roman\PycharmProjects\glaive\projects\FTL-Multiverse 5.3 playtest" /COPYALL /E
+robocopy  "C:\Users\roman\PycharmProjects\glaive\projects\FTL-Multiverse 5.3 - BACKUP" "C:\Users\roman\PycharmProjects\glaive\projects\FTL-Multiverse 5.3" /COPYALL /E
