90e75c68e9d131e0d30b544d25afb7a5ee6d1b6f
Epirasque <romanhable@web.de>
2022-08-27 22:33:14 +0200

    new slipstream path (installer)

--- zipTccFolderIntoSlipstream.bat
+++ zipTccFolderIntoSlipstream.bat
@@ -1,2 +1,2 @@
-move /y "C:\slipstream_for_FTL_mods\mods\TCC LOCAL.zip" "C:\slipstream_for_FTL_mods\mods\TCC LOCAL PREVIOUS.zip"
-"C:\Program Files\7-Zip\7z.exe" a -y "C:\slipstream_for_FTL_mods\mods\TCC LOCAL.zip" "C:\Users\roman\PycharmProjects\glaive\projects\TCC-main\*"
+move /y "C:\Program Files (x86)\Steam\steamapps\common\FTL Faster Than Light\mods\TCC LOCAL.zip" "C:\Program Files (x86)\Steam\steamapps\common\FTL Faster Than Light\mods\TCC LOCAL PREVIOUS.zip"
+"C:\Program Files\7-Zip\7z.exe" a -y "C:\Program Files (x86)\Steam\steamapps\common\FTL Faster Than Light\mods\TCC LOCAL.zip" "C:\Users\roman\PycharmProjects\glaive\projects\TCC-main\*"
