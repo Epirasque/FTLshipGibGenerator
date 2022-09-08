56bd82052778d7d5afbbcc0db23fd94eae45fcfe
Epirasque <romanhable@web.de>
2022-09-08 22:35:59 +0200

    Support ships_noglow subfolder

--- fileHandling/ShipImageLoader.py
+++ fileHandling/ShipImageLoader.py
@@ -15,23 +15,26 @@ VISIBLE_ALPHA_THRESHOLD = 255
 def loadShipBaseImage(shipImageName, sourceFolderpath):
     logger = getSubProcessLogger()
     try:
-        imageArray = imread(
-            sourceFolderpath + '\\img\\ships_glow\\' + shipImageName + BASE_SUFFIX + '.png')
-        # glow should be ignored, is treated as completely transparent
-        imageArray[imageArray[:, :, 3] < VISIBLE_ALPHA_THRESHOLD] = 0
-        # only overwrite alpha, not the colors
-        visiblePoints = np.where(imageArray[:, :, 3] >= VISIBLE_ALPHA_THRESHOLD)
-        imageArray[visiblePoints[0], visiblePoints[1], 3] = 255
+        imageArray = prepareShipImage("ship_glow", shipImageName, sourceFolderpath)
         return imageArray, "ships_glow"
     except FileNotFoundError:
         try:
-            imageArray = imread(
-                sourceFolderpath + '\\img\\ship\\' + shipImageName + BASE_SUFFIX + '.png')
-            # glow should be ignored, is treated as completely transparent
-            imageArray[imageArray[:, :, 3] < VISIBLE_ALPHA_THRESHOLD] = 0
-            # only overwrite alpha, not the colors
-            visiblePoints = np.where(imageArray[:, :, 3] >= VISIBLE_ALPHA_THRESHOLD)
-            imageArray[visiblePoints[0], visiblePoints[1], 3] = 255
+            imageArray = prepareShipImage("ship", shipImageName, sourceFolderpath)
             return imageArray, "ship"
         except FileNotFoundError:
-            logger.error('No image found for shipBlueprint img attribute: %s' % shipImageName)
+            try:
+                imageArray = prepareShipImage("ships_noglow", shipImageName, sourceFolderpath)
+                return imageArray, "ships_noglow"
+            except FileNotFoundError:
+                logger.error('No image found for shipBlueprint img attribute: %s' % shipImageName)
+
+
+def prepareShipImage(shipSubfolderName, shipImageName, sourceFolderpath):
+    imageArray = imread(
+        sourceFolderpath + '\\img\\' + shipSubfolderName + '\\' + shipImageName + BASE_SUFFIX + '.png')
+    # glow should be ignored, is treated as completely transparent
+    imageArray[imageArray[:, :, 3] < VISIBLE_ALPHA_THRESHOLD] = 0
+    # only overwrite alpha, not the colors
+    visiblePoints = np.where(imageArray[:, :, 3] >= VISIBLE_ALPHA_THRESHOLD)
+    imageArray[visiblePoints[0], visiblePoints[1], 3] = 255
+    return imageArray
